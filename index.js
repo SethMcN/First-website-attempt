76f810063cedd31b7ae62d1f45e998f1791e9c1f
SethM <107069935+SethMcN@users.noreply.github.com>
2023-11-25 16:00:34 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-const button = document.querySelector("input");
-const paragraph = document.querySelector("p");
+var canvas = document.getElementById('paint');
+var ctx = canvas.getContext('2d');
+ 
+var sketch = document.getElementById('sketch');
+var sketch_style = getComputedStyle(sketch);
+canvas.width = 700;
+canvas.height = 350;
 
-button.addEventListener("click", updateButton);
+var mouse = {x: 0, y: 0};
+ 
+/* Mouse Capturing Work */
+canvas.addEventListener('mousemove', function(e) {
+  mouse.x = e.pageX - this.offsetLeft;
+  mouse.y = e.pageY - this.offsetTop;
+}, false);
 
-function updateButton() {
-  if (button.value === "Start machine") {
-    button.value = "Stop machine";
-    paragraph.textContent = "The machine has started!";
-  } else {
-    button.value = "Start machine";
-    paragraph.textContent = "The machine is stopped.";
-  }
-}
+/* Drawing on Paint App */
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+ctx.strokeStyle = "blue";
+function getColor(colour){ctx.strokeStyle = colour;}
+
+function getSize(size){ctx.lineWidth = size;}
+
+
+//ctx.strokeStyle = 
+//ctx.strokeStyle = document.settings.colour[1].value;
+ 
+canvas.addEventListener('mousedown', function(e) {
+    ctx.beginPath();
+    ctx.moveTo(mouse.x, mouse.y);
+ 
+    canvas.addEventListener('mousemove', onPaint, false);
+}, false);
+ 
+canvas.addEventListener('mouseup', function() {
+    canvas.removeEventListener('mousemove', onPaint, false);
+}, false);
+ 
+var onPaint = function() {
+    ctx.lineTo(mouse.x, mouse.y);
+    ctx.stroke();
+};
